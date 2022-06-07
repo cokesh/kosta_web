8118d95437d78ab3832cab2bdbd57a9191effcc0
cokesh <spdlqjlwjs@daum.net>
2022-06-07 12:49:41 +0900

    modify addEventListener - for-> forEach

--- js/event.js
+++ js/event.js
@@ -12,15 +12,20 @@ window.addEventListener("load", function() {
     // 계산기 스타트 --------------------
     // DOM 트리에서 class 속성값이 calculator인 객체의 자식 중 모든 button 찾기
     var btnArr = document.querySelectorAll("div.calculator>button"); // 모든 버튼을 찾아오기 때문에 배열타입으로 반환이 됨
-    for ( var i = 0; i < btnArr.length; i++ ) {
-        (function(j) { // i의 값이 진행될때마다 함수가 실행이 되면서 그때 당시의 값을 얻을수 있음
-            btnArr[i].addEventListener('click', function() { // 클릭되었을때 예약을 걸어두는 것이기 때문에 지금당장의 i의 값이 전달이 되지 않고 다 계산이 된후의 i의 값이 들어간다. scope의 개념에 의함. but 하나의 scope을 추가함으로써 원래는 i가 다 증가하고 12의 값이 들어가지만 내부적으로 addEventListener가 진행이 될때마다 함수를 직접 호출함으로써 그때 당시의 i값을 전달하여 콘솔에 찍힐수 있게끔 함.
-                console.log(j+1, '버튼이 클릭됨'); 
-            });
-        })(i);
-    }// 계산기 end --------------------------------
 
+    // for ( var i = 0; i < btnArr.length; i++ ) {
+    //     (function(j) { // i의 값이 진행될때마다 함수가 실행이 되면서 그때 당시의 값을 얻을수 있음
+    //         btnArr[i].addEventListener('click', function() { // 클릭되었을때 예약을 걸어두는 것이기 때문에 지금당장의 i의 값이 전달이 되지 않고 다 계산이 된후의 i의 값이 들어간다. scope의 개념에 의함./ but 하나의 scope을 추가함으로써 원래는 i가 다 증가하고 12의 값이 들어가지만 내부적으로 addEventListener가 진행이 될때마다 함수를 직접 호출함으로써 그때 당시의 i값을 전달하여 콘솔에 찍힐수 있게끔 함.
+    //             console.log(j+1, '버튼이 클릭됨'); 
+    //         });
+    //     })(i);
 
+    btnArr.forEach(function(item, index) { // 배열쓸때 forEach 쓰는 것이 훨 낫다(0-끝까지 순차처리 할 때 쓰길 권장함!!)
+        item.addEventListener('click', function() {
+            console.log(index+1, '클릭!')
+        });
+    });
+    // 계산기 end --------------------------------
 
 });
 
