630af8faee9fc6978cbe4d50c6b5babf29bc6026
cokesh <spdlqjlwjs@daum.net>
2022-06-07 12:35:53 +0900

    add addEventListener-complete scope

--- js/event.js
+++ js/event.js
@@ -13,11 +13,12 @@ window.addEventListener("load", function() {
     // DOM 트리에서 class 속성값이 calculator인 객체의 자식 중 모든 button 찾기
     var btnArr = document.querySelectorAll("div.calculator>button"); // 모든 버튼을 찾아오기 때문에 배열타입으로 반환이 됨
     for ( var i = 0; i < btnArr.length; i++ ) {
-        
-        btnArr[i].addEventListener('click', function() { //클릭되었을때 예약을 걸어두는 것이기 때문에 지금당장의 i의 값이 전달이 되지 않고 다 계산이 된후의 i의 값이 들어간다.
-            console.log(i, '버튼이 클릭됨');
-        });
-    }
+        (function(j) { // i의 값이 진행될때마다 함수가 실행이 되면서 그때 당시의 값을 얻을수 있음
+            btnArr[i].addEventListener('click', function() { // 클릭되었을때 예약을 걸어두는 것이기 때문에 지금당장의 i의 값이 전달이 되지 않고 다 계산이 된후의 i의 값이 들어간다. scope의 개념에 의함. but 하나의 scope을 추가함으로써 원래는 i가 다 증가하고 12의 값이 들어가지만 내부적으로 addEventListener가 진행이 될때마다 함수를 직접 호출함으로써 그때 당시의 i값을 전달하여 콘솔에 찍힐수 있게끔 함.
+                console.log(j+1, '버튼이 클릭됨'); 
+            });
+        })(i);
+    }// 계산기 end --------------------------------
 
 
 
